b42f552c38b15413e06dba9367732b853f856551
Md Yeasin Arafat <mdyeasinhn@gmail.com>
2026-05-11 22:52:57 +0600

    feat: add socket.io-client integration and implement connection/disconnection handling

--- frontend/src/hooks/useSocket.js
+++ frontend/src/hooks/useSocket.js
@@ -1,20 +1,42 @@
+import { useState } from 'react';
 import { useEffect, useRef } from 'react';
 import io from 'socket.io-client';
 
 const SOCKET_URL = import.meta.env.VITE_SOCKET_URL
 
-const useSocket = () => {
+export const useSocket = () => {
     const socketRef = useRef(null);
     const [connected, setConnected] = useState(false);
     useEffect(() => {
         //CREATE SOCKET CONNECTION
         socketRef.current = io(SOCKET_URL, {
             transports: ['websocket', 'polling'],
-        });     
+        });
         //connetion event
         socketRef.current.on('connect', () => {
             setConnected(true);
             console.log('Connected to socket server', socketRef.current.id);
-        })
-}, []);
+        });
+
+        //disconnection event
+        socketRef.current.on('disconnect', () => {
+            setConnected(false);
+            console.log('Disconnected from socket server');
+        });
+
+        socketRef.current.on("connected", (data) => {
+            console.log("server message:", data.message);
+        });
+
+        //CLEANUP ON UNMOUNT
+        return () => {
+            if (socketRef.current) {
+                socketRef.current.disconnect();
+            }
+        };
+    }, []);
+    return {
+        socket : socketRef.current,
+        connected
+    }
 }
